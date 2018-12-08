9a752bc2a29f3fe077460aa1e862218fea517bef
wangzhc <zhengcaowang@163.com>
2018-12-08 19:59:08 +0800

    补充注释

--- microservice-consumer-movie-feign/src/main/java/com/itmuch/cloud/controller/MovieController.java
+++ microservice-consumer-movie-feign/src/main/java/com/itmuch/cloud/controller/MovieController.java
@@ -1,12 +1,11 @@
 package com.itmuch.cloud.controller;
 
+import com.itmuch.cloud.entity.User;
+import com.itmuch.cloud.feign.UserFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.itmuch.cloud.entity.User;
-import com.itmuch.cloud.feign.UserFeignClient;
 
 @RestController
 public class MovieController {
